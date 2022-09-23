9f2fd9bb6fb8f5a8db44a0042a0584d4a2e6e0f1
Cristian Gallego <cris.gahur@hotmail.com>
2022-09-23 18:20:06 -0500

    Google sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,51 +1,102 @@
 const { response } = require("express");
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
+
 const Usuario = require("../models/usuario");
+
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+
 
 const login = async (req, res = response) => {
+  
+  const { correo, password } = req.body;
 
-    const {correo, password} = req.body;
-
-    // Verificar que el correo existe:
-    try {
-        const usuario = await Usuario.findOne({correo});
-        if(!usuario){ // Hacerlo asi es igual que hacer la expresión usuario == false
-            return res.status(400).json({
-                msg: "Usuario / Password no son correctos - correo"
-            })
-        }
+  // Verificar que el correo existe:
+  try {
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      // Hacerlo asi es igual que hacer la expresión usuario == false
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
+    }
 
     // Si el usuario está activo:
-    if (!usuario.estado){
-        return res.status(400).json({
-            msg: "Usuario / Password no son correctos - estado: false"
-        });
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - estado: false",
+      });
     }
 
     // Verificar contraseña:
-    const validPassword = bcryptjs.compareSync(password, usuario.password)
-    if(!validPassword){
-        return res.status(400).json({
-            msg: "Usuario / Password no son correctos - password"
-        });
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - password",
+      });
     }
 
     // Genera el JWT (Jason Web Token):
-    const token = await generarJWT(usuario.id)
+    const token = await generarJWT(usuario.id);
 
-    res.json ({
-        usuario,
-        token
-     })
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            msg: 'Hable con el administrador'
-        })
-        
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+// Funcion que se encarga de recibir el token, y a partir del token destructurar el nombre, img y correo.
+const googleSingIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const {correo, nombre, img} = await googleVerify(id_token);
+    console.log(googleUser);
+
+    let usuario = await Usuario.findOne({correo}); // Esta pidiendo que busque en la BD el correo
+
+    // Si el correo no existe en la BD, lo creamos:
+    if (!usuario) {
+      const data = {
+        nombre,
+        correo,
+        password: ':P',
+        img,
+        google: true
+      };
+
+      usuario = new Usuario( data ); // Aca estamos creando el nuevo usuario con los datos de la cuenta google
+      await usuario.save();
     }
-}
 
-module.exports = {login}
+    // Si el usuario en BD está en estado false / desactivado (porque el admin no se quiere permitir su ingreso):
+
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Hable con el admin, el usuario está bloqueado",
+      });
+    }
+
+    // Genera el JWT (Jason Web Token):
+    const token = await generarJWT(usuario.id); // usuario.id hace referencia la ID que genera mongo.
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "El token de Google no es valido",
+    });
+  }
+};
+
+module.exports = { 
+  login, 
+  googleSingIn }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express"); // Router es una propiedad de express
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth");
+const { login, googleSingIn } = require("../controllers/auth");
 const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
@@ -11,5 +11,10 @@ router.post('/login',[
     check('password', 'La contraseña es obligatorio').not().isEmpty(),
     validarCampos
 ], login); // El segundo parametro es la función que está en controlador.
+
+router.post('/google',[
+    check('id_token', 'Id_token de Google es necesario').not().isEmpty(),
+    validarCampos
+], googleSingIn); // El segundo parametro es la función que está en controlador.
 
 module.exports = router;
